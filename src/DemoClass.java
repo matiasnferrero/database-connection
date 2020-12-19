4e3acd58fd60ab828a677545945731ed42574cff
Matias <matiasnferrero@gmail.com>
2020-12-19 17:28:48 -0300

    refactoring to make the code leaner

--- src/DemoClass.java
+++ src/DemoClass.java
@@ -9,34 +9,31 @@ public class DemoClass {
         try {
 
             Scanner sc = new Scanner(System.in);
+
             System.out.println("enter username");
             String uname = sc.next();
 
-            Scanner sc2 = new Scanner(System.in);
             System.out.println("enter pass");
-            String pass = sc2.next();
+            String pass = sc.next();
 
-            Scanner sc3 = new Scanner(System.in);
             System.out.println("enter connection string");
-            String url = sc3.nextLine();
+            String url = sc.next();
 
-            Scanner sc4 = new Scanner(System.in);
             System.out.println("enter query");
-            String query = sc4.nextLine();
+            sc.nextLine(); //this is to move the cursor to the next line
+            String query = sc.nextLine();
 
 
             System.out.println("your username is: " + uname);
             System.out.println("connection string: " + url);
 
             boolean isFound = query.contains("LIMIT");
+            
 
-            if (isFound) {
-                System.out.println("Running query: " + query);
-            } else {
+            if (isFound==false) {
                 int MAX_RESULTS = 5;
                 System.out.println("you didnt pass a LIMIT. Setting LIMIT to " + MAX_RESULTS + " to not overload the RAM");
                 query = query + " LIMIT " + MAX_RESULTS;
-                System.out.println(query);
             }
 
 
@@ -44,7 +41,7 @@ public class DemoClass {
             Connection con = DriverManager.getConnection(url,uname,pass);
             Statement st = con.createStatement();
 
-            System.out.println("Querying the database...");
+            System.out.println("Running query: " + query);
             ResultSet rs = st.executeQuery(query);
             rs.next();
 
